f6fd34f609f5d2ee45b36c435f14dc17c4994f9b
Anke Jäger <anke_jager@peopleware.be>
2021-12-13 20:59:14 +0100

    Simplified solution for day 13

--- AdventOfCode/Day_13.cs
+++ AdventOfCode/Day_13.cs
@@ -13,7 +13,7 @@ namespace AdventOfCode
         private const string Horizontal = "HORIZONTAL";
         private const string Vertical = "VERTICAL";
 
-        private readonly bool[,] _dots;
+        private readonly HashSet<(int row, int col)> _dots;
         private readonly List<(string fold, int coordinate)> _folds;
 
         public Day_13()
@@ -23,108 +23,81 @@ namespace AdventOfCode
                 .ReadAllLines(InputFilePath)
                 .ToArray();
 
-            var dots = input
+            _dots = input
                 .Select(i => regexDots.Match(i))
                 .Where(m => m.Success)
-                .Select(m => new
-                {
-                    row = int.Parse(m.Groups["row"].Value),
-                    col = int.Parse(m.Groups["col"].Value)
-                })
-                .ToList();
-            int maxRow = dots.Max(d => d.row);
-            int maxCol = dots.Max(d => d.col);
-            _dots = new bool[maxRow + 1, maxCol + 1];
-
-            foreach (var dot in dots)
-            {
-                _dots[dot.row, dot.col] = true;
-            }
+                .Select(m => (int.Parse(m.Groups["row"].Value), int.Parse(m.Groups["col"].Value)))
+                .ToHashSet();
 
             Regex regexFolds = new Regex("fold along (?<foldtype>x|y)=(?<lineNbr>\\d+)");
-            var folds = input
+            _folds = input
                 .Select(i => regexFolds.Match(i))
                 .Where(m => m.Success)
-                .Select(m => new
-                {
-                    foldType = m.Groups["foldtype"].Value,
-                    lineNbr = int.Parse(m.Groups["lineNbr"].Value)
-                })
+                .Select(m => (m.Groups["foldtype"].Value == "y" ? Horizontal : Vertical, int.Parse(m.Groups["lineNbr"].Value)))
                 .ToList();
-            _folds = new List<(string fold, int coordinate)>();
-            foreach (var fold in folds)
-            {
-                string foldType = fold.foldType == "y" ? Horizontal : Vertical;
-                _folds.Add((foldType, fold.lineNbr));
-            }
         }
 
         public override ValueTask<string> Solve_1()
         {
             // only first fold
             (string foldType, int lineNbr) fold = _folds.First();
-            bool[,] afterFold = DoFold(fold, _dots);
-            int count = afterFold.Cast<bool>().Count(af => af);
+            HashSet<(int, int)> afterFold = DoFold(fold, _dots);
 
-            return new ValueTask<string>($"{count}");
+            return new ValueTask<string>($"{afterFold.Count}");
         }
 
         public override ValueTask<string> Solve_2()
         {
-            bool[,] afterFold = _folds.Aggregate(_dots, (current, fold) => DoFold(fold, current));
-            for (int rowIdx = 0; rowIdx < afterFold.GetLength(0); rowIdx++)
+            HashSet<(int row, int col)> afterFold = new HashSet<(int row, int col)>(_dots);
+            foreach ((string fold, int coordinate) fold in _folds)
             {
-                for (int colIdx = 0; colIdx < afterFold.GetLength(1); colIdx++)
-                {
-                    Console.Write($"{(afterFold[rowIdx, colIdx] ? "#" : ".")}");
-                }
+                afterFold = DoFold(fold, afterFold);
+            }
 
+            int nbrRows = afterFold.Max(af => af.row);
+            int nbrCols = afterFold.Max(af => af.col);
+
+            for (int rowIdx = 0; rowIdx <= nbrRows; rowIdx++)
+            {
+                for (int colIdx = 0; colIdx <= nbrCols; colIdx++)
+                {
+                    Console.Write($"{(afterFold.Contains((rowIdx, colIdx)) ? "#" : ".")}");
+                }
                 Console.WriteLine();
             }
 
             return new ValueTask<string>("See what nice string is printed");
         }
 
-        private static bool[,] DoFold((string foldType, int lineNbr) fold, bool[,] currentDots)
+        private static HashSet<(int row, int col)> DoFold((string foldType, int lineNbr) fold, HashSet<(int row, int col)> currentDots)
         {
-            bool[,] result;
-            int currentNbrRows = currentDots.GetLength(0);
-            int currentNbrCols = currentDots.GetLength(1);
+            HashSet<(int row, int col)> result = new HashSet<(int row, int col)>();
+
             if (fold.foldType == Vertical)
             {
-                int nbrCols = Math.Max(fold.lineNbr, currentNbrCols - fold.lineNbr - 1);
-                result = new bool[currentNbrRows, nbrCols];
-                for (int rowIdx = 0; rowIdx < currentNbrRows; rowIdx++)
+                foreach ((int row, int col) dot in currentDots)
                 {
-                    for (int colOffset = 1;
-                        fold.lineNbr - colOffset >= 0 || fold.lineNbr + colOffset < currentNbrCols;
-                        colOffset++)
+                    if (dot.col < fold.lineNbr)
                     {
-                        int minColIdx = fold.lineNbr - colOffset;
-                        int maxColIdx = fold.lineNbr + colOffset;
-                        bool b = minColIdx >= 0 && currentDots[rowIdx, minColIdx];
-                        b |= maxColIdx < currentNbrCols && currentDots[rowIdx, maxColIdx];
-
-                        result[rowIdx, minColIdx] = b;
+                        result.Add(dot);
+                    }
+                    else if (dot.col > fold.lineNbr)
+                    {
+                        result.Add((dot.row, fold.lineNbr - (dot.col - fold.lineNbr)));
                     }
                 }
             }
             else
             {
-                int nbrRows = Math.Max(fold.lineNbr, currentNbrRows - fold.lineNbr - 1);
-                result = new bool[nbrRows, currentNbrCols];
-                for (int rowOffset = 1;
-                    fold.lineNbr - rowOffset >= 0 || fold.lineNbr + rowOffset < currentNbrRows;
-                    rowOffset++)
+                foreach ((int row, int col) dot in currentDots)
                 {
-                    for (int colIdx = 0; colIdx < currentNbrCols; colIdx++)
+                    if (dot.row < fold.lineNbr)
                     {
-                        int minRowIdx = fold.lineNbr - rowOffset;
-                        int maxRowIdx = fold.lineNbr + rowOffset;
-                        bool b = minRowIdx >= 0 && currentDots[minRowIdx, colIdx];
-                        b |= maxRowIdx < currentNbrRows && currentDots[maxRowIdx, colIdx];
-
-                        result[minRowIdx, colIdx] = b;
+                        result.Add(dot);
+                    }
+                    else if (dot.row > fold.lineNbr)
+                    {
+                        result.Add((fold.lineNbr - (dot.row - fold.lineNbr), dot.col));
                     }
                 }
             }
